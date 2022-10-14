a45f92d5f3af78085a000ac0d5a37c72410d82a6
Philip Berg <pb1015@msstate.edu>
2022-10-14 11:59:15 +0200

    Changed integration to parameteric to increase speed.

--- R/sample_posterior.R
+++ R/sample_posterior.R
@@ -88,7 +88,7 @@ utils::globalVariables(c("alpha", "betau", "id", "tmp", "intu", "condi"))
 #'                  # this will greatly reduce the speed of running baldur
 #'   )
 #' }
-sample_posterior <- function(data, id_col_name, design_matrix, contrast_matrix, uncertainty_matrix, bayesian_model = stanmodels$uncertainty_model, clusters = 1, robust = FALSE, perc = .05, mu_not = 0, ...){
+sample_posterior <- function(data, id_col_name, design_matrix, contrast_matrix, uncertainty_matrix, bayesian_model = stanmodels$uncertainty_model_semi_informative, clusters = 1, robust = FALSE, perc = .05, mu_not = 0, ...){
   rstan_inputs <- rlang::dots_list(...)
   rstan_inputs$verbose <- F
   N <- sum(design_matrix)
@@ -256,8 +256,7 @@ estimate_error <- function(posterior, robust, perc, mu_not) {
     q <- stats::quantile(posterior, c(perc, 1-perc))
     posterior <- posterior[ dplyr::between(posterior, q[1], q[2]) ]
   }
-  e <- stats::ecdf(posterior)
-  p <- e(mu_not)
+  p <- pnorm(mu_not, mean(posterior), sd(posterior))
   if (p < .5) {
     2 * p
   }
